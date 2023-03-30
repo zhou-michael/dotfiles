aedcdadde4d4db440defa21b8c50e6e620aa7c2c
Michael Zhou <micha.zhou@gmail.com>
2023-03-29 17:40:16 -0700

    fixing lspsaga and mason

--- nvim/lua/config/lspsaga.lua
+++ nvim/lua/config/lspsaga.lua
@@ -1,6 +1,11 @@
 local saga = require("lspsaga")
-saga.init_lsp_saga({
+saga.setup({
     border_style = 'rounded',
-    custom_kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind(),
+    symbol_in_winbar = {
+        enable = false
+    },
+    ui = {
+        kind = require("catppuccin.groups.integrations.lsp_saga").custom_kind()
+    }
 })
 
