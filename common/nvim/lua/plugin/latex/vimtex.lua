0b74ca35d71fb83af9c4a98ba879a33595823f04
Michael Zhou <micha.zhou@gmail.com>
2026-09-11 11:44:26 -0700

    feat(popos): configure zathura with catppuccin mocha theme and vimtex synctex

--- common/nvim/lua/plugin/latex/vimtex.lua
+++ common/nvim/lua/plugin/latex/vimtex.lua
@@ -2,10 +2,13 @@ return {
     "lervag/vimtex",
     lazy = false,
     config = function(_, _)
-        vim.g.vimtex_view_method = "sioyek"
-        vim.g.vimtex_quickfix_mode = 1
-        vim.g.vimtex_view_skim_sync = 1
-        vim.g.vimtex_view_skim_reading_bar = 1
+        if vim.fn.has("mac") == 1 then
+            vim.g.vimtex_view_method = "sioyek"
+            vim.g.vimtex_view_skim_sync = 1
+            vim.g.vimtex_view_skim_reading_bar = 1
+        else
+            vim.g.vimtex_view_method = "zathura"
+        end
         vim.g.vimtex_compiler_latexmk = {
             out_dir = "build",
         }
